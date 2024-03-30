629196ea52800bc79361a9aff2fece2e009c7bf5
Flinmt <vitorlimagerr.profissional@gmail.com>
2024-03-30 19:24:52 -0300

    feat: funcionario page

---
+++ src/app/funcionario/page.js
@@ -0,0 +1,12 @@
+import { Navbar } from "@/components/navbar"
+
+export default function Funcioario() {
+    return (
+        <>
+            <Navbar />
+            <main className=" mt-5 md:ml-14 w-[95%]">
+                asd
+            </main>
+        </>
+    )
+}
