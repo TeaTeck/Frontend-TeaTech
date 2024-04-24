ece06870091af70ff125e49a8353e707ccba2154
Lucas Brito <113482408+LucasBriito@users.noreply.github.com>
2024-04-23 22:33:36 -0300

    Mudanças nos botões de salvar e voltar

--- src/app/cadastro/funcionario/page.js
+++ src/app/cadastro/funcionario/page.js
@@ -7,7 +7,7 @@ export default function Funcionario() {
             <main className="mt-5 md:ml-14 w-[95%]">
                 <h2 className="md:ml-24 ml-5 mb-10 font-bold text-2xl">Cadastrar Funcionario</h2>
                 <form className="w-full flex gap-5 items-center pb-5">
-                    <div className="w-full ml-40 grid grid-cols-2 gap-x-0 gap-y-9">
+                    <div className="w-full ml-40 grid grid-cols-2 gap-x-0 gap-y-10">
                         <section className="w-[70%]">
                             <label className="font-bold ml-2">Nome completo</label>
                             <div className="w-full p-[10px] border border-gray-700 rounded-lg bg-white shadow-md">
@@ -38,7 +38,7 @@ export default function Funcionario() {
                             </div>
                         </section>
                     </div>
-                    <div className="flex justify-center fixed bottom-0 left-0 w-full">
+                    <div className="flex justify-center fixed bottom-2 left-0 w-full">
                         <button className="bg-[#3082B0] p-5 rounded-lg text-white font-semibold text-xl shadow-md hover:bg-white hover:text-[#01499C] transition duration-300 mr-5">
                             Voltar
                         </button>
